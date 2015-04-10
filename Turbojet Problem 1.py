14921035152f630cc78450c5ebb36c33d127028b
Grayson Savage <graysonsavage@yahoo.com>
2015-04-10 01:23:54 -0400

    Fixed Errors

--- Turbojet Problem 1.py
+++ Turbojet Problem 1.py
@@ -7,7 +7,7 @@ import numpy as np
 # Conditions given in the problem
 Pa = 12.112
 Ta = 216.65
-To4_max
+To4_max = 1500
 gamma1 = 1.4
 gamma2 = 1.3
 R = 287
@@ -26,14 +26,14 @@ nt=0.92
 nn=0.98
  
 
-Mlist = np.array([])
-Ilist = np.array([])
-TSFClist = np.array([])
-nthlist = np.array([])
-nplist = np.array([])
-nolist = np.array([])
-rclist = np.array([])
-A_ratiolist = np.array([])
+Mlist = []
+Ilist = []
+TSFClist = []
+nthlist = []
+nplist = []
+nolist = []
+rclist = []
+A_ratiolist = []
 
 #Flow Conditions
 Toa = Ta*(1 + ((gamma1-1)/2)*M**2)
@@ -45,9 +45,11 @@ print u_in
 
 #Inlet/Diffuser
 To2=Toa
-To2s=nd*(To2-Ta)-Ta
-print To2s
+To2s=nd*(To2-Ta)+Ta
+print 'To2s '
+print 'To2s '
 Po2=Pa*(To2s/Ta)**(gamma1/(gamma1-1))
+print 'Po2 '
 print Po2
 
 #Compressor
@@ -55,69 +57,83 @@ for rc in np.arange(2,60,0.1):
     To4 = To4_max
     
     To3s=To2*rc**((gamma1-1)/gamma1)
-	if rc == 2:
-		print To3s
-	Po3=rc*Po2
-	if rc == 2:
-		print Po3
-	To3 = ((To3s-To2)/nc)+To2
-	if rc == 2:
-		print To3
-	wc_in = Cp1*(To3-To2)
-	if rc == 2:
-		print wc_in
+    if rc == 2:
+        print 'To3s '
+        print To3s
+    Po3=rc*Po2
+    if rc == 2:
+        print 'Po3 '
+        print Po3
+    To3 = ((To3s-To2)/nc)+To2
+    if rc == 2:
+    	print 'To3 '
+    	print To3
+    wc_in = Cp1*(To3-To2)
+    if rc == 2:
+        print 'wc_in '
+        print wc_in
 	
-	#Combustor
-	Fb=(((To4/To3)-1)/((nb*hc/(Cp2*To3))-(To4/To3)))
-	if Fb >= Fst:
-		Fb = Fst
-		To4 = (Fb*nb*hc/(Cp2)+Toa)/(1+Fb)
-	Po4=Po3
+    #Combustor
+    Fb=(((To4/To3)-1)/((nb*hc/(Cp2*To3))-(To4/To3)))
+    if Fb >= Fst:
+    	Fb = Fst
+    	To4 = (Fb*nb*hc/(Cp2)+Toa)/(1+Fb)
+    Po4=rc*Po2
+    if rc == 2:
+        print 'Fb '
+        print Fb
 	
-	#Turbine
-	wt_out=wc_in
-	To5=To4-(wt_out/(cp*(1+Fb)))
-	if rc == 2:
-		print To5
-	To5s=To4-((To4-To5)/nt)
-	if rc == 2:
-		print To5s
-	Po5=Po4*(To5s/To4)**(gamma2/gamma2-1)
-	if rc == 2:
-		print Po5
-	
-	#Nozzle
-	To6=To5
-	To7=To6
-	Po6=Po5
-	P7=Pa
-	T7as=(To6/((Po6/P7)**(gamma2-1/gamma2)))
-	if rc == 2:
-		print T7as
-	T7=To6-nn*(To6-T7as)
-	if rc == 2:
-		print T7
-	M7=math.sqrt(((To7/T7)-1)*(2/(gamma2-1)))
-	if rc == 2:
-		print M7
-	u7 = M7*math.sqrt(gamma2*R*T7)
-	if rc == 2:
-		print u7
+    #Turbine
+    wt_out=wc_in
+    To5=To4-(wt_out/(Cp2*(1+Fb)))
+    if rc == 2:
+        print 'To5 '
+        print To5
+    To5s=To4-((To4-To5)/nt)
+    if rc == 2:
+        print 'To5s '
+        print To5s
+    Po5=Po4*(To5s/To4)**(gamma2/gamma2-1)
+    if rc == 2:
+    	print 'Po5 '
+    	print Po5
+    
+    #Nozzle
+    To6=To5
+    To7=To6
+    Po6=Po5
+    P7=Pa
+    T7as=(To6/((Po6/P7)**(gamma2-1/gamma2)))
+    if rc == 2:
+    	print 'T7as '
+    	print T7as
+    T7=To6-nn*(To6-T7as)
+    if rc == 2:
+    	print 'T7 '
+    	print T7
+    M7=math.sqrt(((To7/T7)-1)*(2/(gamma2-1)))
+    if rc == 2:
+    	print 'M7 '
+    	print M7
+    u7 = M7*math.sqrt(gamma2*R*T7)
+    if rc == 2:
+        print 'u7 '
+        print u7
 		
     I = (1+Fb)*u7-u_in
     TSFC = Fb/I
-	nth=(((1+Fb)*u7**2-u_in**2)/(2*Fb*hc*1000))
-	np=(2*u_in/(u7+u_in))
-	no=nth*no
-	A_ratio = (1/M7)*((2/2.3)*(1+(0.3/2)*M7**2))**(2.3/0.6)
+    nth=(((1+Fb)*u7**2-u_in**2)/(2*Fb*hc*1000))
+    np=(2*u_in/(u7+u_in))
+    no=nth*np
+    A_ratio = (1/M7)*((2/2.3)*(1+(0.3/2)*M7**2))**(2.3/0.6)
 	
-    Ilist=np.append(Ilist,I)
-    TSFClist=np.append(TSFClist,TSFC)
-    A_ratiolist=np.append(A_ratiolist,A_ratio) 
-	nthlist=np.append(nthlist,nth)
-	nplist=np.append(nplist,np)
-	nolist=np.append(nolist,no)
-	rclist=np.append(rclist,rc)
+    Ilist.append([I])
+    TSFClist.append([TSFC])
+    A_ratiolist.append([A_ratio])
+    nthlist.append([nth])
+    nplist.append([np])
+    nolist.append([no])
+    rclist.append([rc])
 	
     
 # Now to plot everything!
